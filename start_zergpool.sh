1a6d4dfe5f94e9ba526266351214f5ebd396ba56
Pokeu12 <abdurrahmanathofani8@gmail.com>
2024-09-28 16:25:44 +0700

    Update start_zergpool.sh

--- start_zergpool.sh
+++ start_zergpool.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cpu_threads=1
+cpu_threads=2
 echo "Start SRBMiner-MULTI with Parameters: --algorithm $ALGO --pool $POOL_ADDRESS --wallet LTC:$WALLET_USER.$HOSTNAME#Jumper --password $PASSWORD $EXTRAS --cpu-threads $cpu_threads"
 ./SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet LTC:$WALLET_USER.$HOSTNAME#Jumper --password $PASSWORD $EXTRAS --cpu-threads $cpu_threads
